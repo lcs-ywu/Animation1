60a90975880922696e19c143447cf0962f3a65f2
James Wu <ywu@lcs.on.ca>
2020-04-16 23:02:58 +0800

    Made a mistake that causes all the lines move in the same direction

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -15,21 +15,52 @@ class Sketch : NSObject {
         
         // Create canvas object – specify size
         canvas = Canvas(width: 500, height: 500)
-        
         // Set starting position
         x = 250
+//        // Change the fill to green
+//        canvas.fillColor = Color.green
+        canvas.drawShapesWithBorders = false
         
     }
     
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
         
-        // Change position
-        x += 1
+//        // Change position
+//        x += 1
+//        // Print the value of x in the debug console
+//        print(x)
+//        // Draw an ellipse in the middle of the canvas
+//        canvas.drawEllipse(at: Point(x: x, y: 250), width: 50, height: 50)
+        for y in stride(from: 50, through: 450, by: 100){
+            if y == 450 {
+                canvas.fillColor = .purple
+                x += 1
+                canvas.drawEllipse(at: Point(x: x, y: y), width: 50, height: 50)
+            }else if y == 350 {
+                canvas.fillColor = .orange
+                x -= 1
+                canvas.drawEllipse(at: Point(x: x, y: y), width: 50, height: 50)
+            }else if y == 250 {
+                canvas.fillColor = .green
+                x += 1
+                canvas.drawEllipse(at: Point(x: x, y: y), width: 50, height: 50)
+            }else if y == 150 {
+                canvas.fillColor = .blue
+                x -= 1
+                canvas.drawEllipse(at: Point(x: x, y: y), width: 50, height: 50)
+            }else {
+                canvas.fillColor = .black
+                x += 1
+                canvas.drawEllipse(at: Point(x: x, y: y), width: 50, height: 50)
+            }
+        }
         
-        // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(at: Point(x: x, y: 250), width: 50, height: 50)
+        
         
     }
+    
+    
+    
     
 }
