670761d0f833b4f0c061037b8b7f5be4c2b7fa23
James Wu <ywu@lcs.on.ca>
2020-04-19 21:51:08 +0800

    Finished bouncing circle challenge, more comments are coming

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,6 +9,8 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
+    var y : Int
+    var z : Int
     
     // This function runs once
     override init() {
@@ -17,18 +19,53 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
-        x = 250
+        x = random(from: 0, to: 500)
+        y = random(from: 0, to: 500)
+        z = 0
+        
+        canvas.drawShapesWithBorders = false
         
     }
     
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
         
+        canvas.fillColor = .white
+        if x<500,y<500 {
+            canvas.drawEllipse(at: Point(x: x, y: y), width: 55, height: 55)
+        }else if x>=500,y<500 {
+            canvas.drawEllipse(at: Point(x: 1000-x, y: y), width: 55, height: 55)
+        }else if x<500,y>=500 {
+            canvas.drawEllipse(at: Point(x: x, y: 1000-y), width: 55, height: 55)
+        }else if x>=500,y>=500 {
+            canvas.drawEllipse(at: Point(x: 1000-x, y: 1000-y), width: 55, height: 55)
+        }
+        
         // Change position
         x += 1
+        y += 1
         
+        canvas.fillColor = .black
         // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(at: Point(x: x, y: 250), width: 50, height: 50)
+        if x<500,y<500 {
+            canvas.drawEllipse(at: Point(x: x, y: y), width: 50, height: 50)
+        }else if x>=500,y<500 {
+            canvas.drawEllipse(at: Point(x: 1000-x, y: y), width: 50, height: 50)
+        }else if x<500,y>=500 {
+            canvas.drawEllipse(at: Point(x: x, y: 1000-y), width: 50, height: 50)
+        }else if x>=500,y>=500 {
+            canvas.drawEllipse(at: Point(x: 1000-x, y: 1000-y), width: 50, height: 50)
+        }
+        
+        if x==1000 {
+            x = 0
+        }
+        if y==1000 {
+            y = 0
+        }
+        
+//        print(x)
+//        print(y)
         
     }
     
